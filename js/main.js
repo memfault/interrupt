3dc61824847100d7cb2e140366c5d9f3aa108010
KingFelix <jacopo.rabolini@gmail.com>
2015-05-03 16:15:39 +0200

    Add reverse menu

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
-var icon = document.getElementById("nav-menu");
+var normal = document.getElementById("nav-menu");
+var reverse = document.getElementById("nav-menu-left");
 
-// Toggle the "menu-open" class
+var icon = normal !== null ? normal : reverse;
+
+// Toggle the "menu-open" % "menu-opn-left" classes
 function toggle() {
-	  var nav = document.getElementById("nav");
+	  var navRight = document.getElementById("nav");
+	  var navLeft = document.getElementById("nav-left");
+	  var nav = navRight !== null ? navRight : navLeft;
+
 	  var button = document.getElementById("menu");
 	  var site = document.getElementById("wrap");
 	  
-	  if (nav.className == "menu-open") {
+	  if (nav.className == "menu-open" || nav.className == "menu-open-left") {
 	  	  nav.className = "";
 	  	  button.className = "";
 	  	  site.className = "";
+	  } else if (reverse !== null) {
+	  	  nav.className += "menu-open-left";
+	  	  button.className += "btn-close";
+	  	  site.className += "fixed";
 	  } else {
 	  	  nav.className += "menu-open";
 	  	  button.className += "btn-close";
@@ -19,10 +29,12 @@ function toggle() {
 
 // Ensures backward compatibility with IE old versions
 function menuClick() {
-	if (document.addEventListener) {
+	if (document.addEventListener && icon !== null) {
 		icon.addEventListener('click', toggle);
-	} else if (document.attachEvent) {
+	} else if (document.attachEvent && icon !== null) {
 		icon.attachEvent('onclick', toggle);
+	} else {
+		return;
 	}
 }
 
