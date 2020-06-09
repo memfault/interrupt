55fcdb196ddb0aab463121c24dbedc5049cab31c
Tyler Hoffman <tyler@memfault.com>
2020-06-09 16:09:02 -0700

    Add firmware-shell example code

---
+++ example/firmware-shell/README.md
@@ -0,0 +1,9 @@
+# Firmware Shell Example
+
+## Setup
+
+```
+$ wget https://developer.nordicsemi.com/nRF5_SDK/nRF5_SDK_v15.x.x/nRF5_SDK_15.3.0_59ac345.zip
+$ unzip nRF5_SDK_15.3.0_59ac345.zip
+$ ln -s nRF5_SDK_15.3.0_59ac345 nrf5_sdk
+```
---
+++ example/firmware-shell/auto_serial.py
@@ -0,0 +1,24 @@
+import serial
+import io
+from serial.tools import list_ports
+
+
+def main():
+    # Connect to a device matching what we want
+    port = list(list_ports.grep(r"/dev/cu.usbmodem*"))[0]
+    ser = serial.Serial(port.device, 115200, timeout=1)
+    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
+    # Write our command and send immediately
+    sio.write("hello Tyler\n")
+    sio.flush()
+
+    # Our serial implementation prints the input to the shell
+    command = sio.readline()
+    # Read the response on the next line
+    response = sio.readline()
+    # output to user
+    print(response)
+
+if __name__ == "__main__":
+    main()
