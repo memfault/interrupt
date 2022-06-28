55b1fa3c79bc2980be0b086eae26a56c02c855d0
koisto <48182316+koisto@users.noreply.github.com>
2022-06-28 19:58:41 +0100

    Update gpio.c
    Setup GPIOA pin 3 as USART2 receive pin. Otherwise the example won't work on real hardware.

--- example/fwup-architecture/gpio.c
+++ example/fwup-architecture/gpio.c
@@ -7,9 +7,14 @@ void gpio_setup(void)
     /* Setup GPIO pin GPIO12 on GPIO port D for LED. */
     gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
 
-    /* Setup GPIO pins for USART2 transmit. */
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+	/* Setup GPIO pins for USART2 transmit. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
 
-    /* Setup USART2 TX pin as alternate function. */
-    gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+	/* Setup GPIO pins for USART2 receive. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO3);
+	gpio_set_output_options(GPIOA, GPIO_OTYPE_OD, GPIO_OSPEED_25MHZ, GPIO3);
+
+	/* Setup USART2 TX and RX pin as alternate function. */
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+	gpio_set_af(GPIOA, GPIO_AF7, GPIO3);
 }
