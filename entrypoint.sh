173a1af6611c5c376dca5d6ca9182764a880930d
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2024-09-21 11:44:34 -0400

    [WIP] memfault sdk esp component article

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 . /venv/bin/activate
-bundle exec jekyll serve --host 0.0.0.0 -D "$@" --trace --future
+bundle exec jekyll serve --host 0.0.0.0 -D "$@" --trace --future --incremental --livereload
